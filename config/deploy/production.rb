47690c90eda971cee8fc88d742be9d1022413399
kashun0410 <blywb32430@yahoo.co.jp>
2020-04-29 00:11:52 +0900

    basic認証

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,6 +7,10 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
+server '18.180.136.64', user: 'ec2-user', roles: %w{app db web}
+
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
 
 
 # role-based syntax
@@ -59,5 +63,3 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-
-server '18.180.136.64', user: 'ec2-user', roles: %w{app db web}
